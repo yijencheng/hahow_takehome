595239f9ccae6442974ac2e21900c110444128f3
yijencheng <ivan861118@gmail.com>
2021-06-23 14:06:08 +0800

    add custom error handler

---
+++ api/error_handler.py
@@ -0,0 +1,10 @@
+from flask.wrappers import Response
+
+
+from flask import Response
+
+def err_response(status_code: int) -> Response:
+    if status_code == 404:
+        return Response("Not found", status=404)
+    else:
+        return Response("Internal Server Error", status=500)
--- api/hahow.py
+++ api/hahow.py
@@ -1,25 +1,33 @@
+from json.decoder import JSONDecodeError
 from flask import Response
 import requests
 import json
+from .error_handler import err_response
 
 def get_heros():
     endpoint = "https://hahow-recruit.herokuapp.com/heroes"
     resp = requests.get(endpoint)
 
+    if resp.status_code != 200:
+        return err_response(resp.status_code)
+    
     try:
         data = resp.json()
-    except Exception:
-        return Response(b"The Returned data is not json decodable", status=500)
+    except JSONDecodeError:
+        return Response("The Returned data is not json decodable", status=500)
 
     return Response(json.dumps(data), status=200, mimetype='application/json')
 
 def get_hero_by_id(hero_id):
     endpoint = f"https://hahow-recruit.herokuapp.com/heroes/{hero_id}"
     resp = requests.get(endpoint)
+    
+    if resp.status_code != 200:
+        return err_response(resp.status_code)
 
     try:
         data = resp.json()
-    except Exception:
-        return Response(b"The Returned data is not json decodable", status=500)
+    except JSONDecodeError:
+        return Response("The Returned data is not json decodable", status=500)
 
     return Response(json.dumps(data), status=200, mimetype='application/json')
