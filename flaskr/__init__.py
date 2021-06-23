6124af2af137ce47152e105f807fc671c9b5ca59
yijencheng <34654196+yijencheng@users.noreply.github.com>
2021-06-23 21:35:46 +0800

    Revert "Add hahow unauthenticate Heroes api, single hero api"

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,26 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource
-
-from api.hahow import get_hero_by_id, get_heros
-
 app = Flask(__name__)
-api = Api(app)
-
-
-@app.route("/")
+@app.route('/')
 def test():
     return "Welcome to Flask!"
-
-
-class HeroList(Resource):
-    def get(self):
-        return get_heros()
-
-
-class Hero(Resource):
-    def get(self, hero_id):
-        return get_hero_by_id(hero_id)
-
-
-api.add_resource(HeroList, "/heroes")
-api.add_resource(Hero, "/heroes/<int:hero_id>")
--- tests/utils.py
+++
@@ -1,7 +0,0 @@
-class MockResponse:
-    def __init__(self, data: dict, status_code: int):
-        self.data = data
-        self.status_code = status_code
-
-    def json(self):
-        return self.data
