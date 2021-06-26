e72238fb0521ddf2c5bd03cc9b14e9de8a64d219
yijencheng <ivan861118@gmail.com>
2021-06-26 11:12:00 +0800

    change unauthorized error-handler response

--- flaskr/error_handler.py
+++ flaskr/error_handler.py
@@ -5,7 +5,9 @@ def err_response(status_code: int) -> Response:
     if status_code == 400:
         return Response("Bad Request", status=400)
     elif status_code == 401:
-        return Response("Unauthorized client", status=401)
+        return Response(
+            "Unauthorized client. Please check your name and password.", status=401
+        )
     elif status_code == 404:
         return Response("Not found", status=404)
     elif status_code == 502:
